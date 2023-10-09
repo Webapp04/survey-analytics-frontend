01d4f1ef50338eea7290a9fd262c93dc5a140860
Webapp04 <expertwebapp04@gmail.com>
2023-10-09 17:09:12 +0530

    Date Range Filter

--- src/containers/Dashboard/index.jsx
+++ src/containers/Dashboard/index.jsx
@@ -1,15 +1,19 @@
 import React, { useState } from "react";
 import useGetSurveyReport from "../../services/apiQueries/dashboard";
-import AnalyticCard from "../../components/AnalyticCard";
 import "./dashboard.css";
 import { Tab, Tabs } from "@mui/material";
 import SurveyTable from "../../components/SurveyTable";
+import dayjs from "dayjs";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DateRangePicker } from "@mui/x-date-pickers-pro/DateRangePicker";
 
 const Dashboard = () => {
   const [tab, setTab] = useState(0);
+  const [value, setValue] = useState();
 
   const { data: surveyReport, loading: surveyReportLoading } =
-    useGetSurveyReport();
+    useGetSurveyReport(value);
 
   const getBusinessType = (field, type) => {
     const businessType = surveyReport?.filter((data) => data?.[field] === type);
@@ -22,10 +26,18 @@ const Dashboard = () => {
 
   return (
     <div className="main-container">
-      <Tabs value={tab} onChange={handleTabChange}>
-        <Tab label="Intrested" />
-        <Tab label="Not Intrested" />
-      </Tabs>
+      <div className="flex-row">
+        <Tabs value={tab} onChange={handleTabChange}>
+          <Tab label="Intrested" />
+          <Tab label="Not Intrested" />
+        </Tabs>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DateRangePicker
+            value={value}
+            onChange={(newValue) => setValue(newValue)}
+          />
+        </LocalizationProvider>
+      </div>
       {tab === 0 && (
         <div className="business-container">
           <div className="business-card">
--- src/services/apiQueries/dashboard.js
+++ src/services/apiQueries/dashboard.js
@@ -1,9 +1,21 @@
 import { useQuery } from "react-query";
 import { api } from "../api";
+import { serialize } from "../../utility/helper";
 
-export default function useGetSurveyReport() {
-  const getReport = () => api.get(`surveys`);
-  return useQuery(`surveys`, getReport, {
+export default function useGetSurveyReport(dateRange) {
+  let query;
+  if (dateRange?.[0] !== null && dateRange?.[1]) {
+    const startDate = new Date(dateRange?.[0]?.$d);
+    const endDate = new Date(dateRange?.[1]?.$d);
+    query = {
+      startDate: startDate,
+      endDate: endDate,
+    };
+  }
+  const queryParam = dateRange ? serialize(query) : serialize();
+  const getReport = () => api.get(`surveys${queryParam}`);
+
+  return useQuery(`surveys${queryParam}`, getReport, {
     enabled: true,
   });
 }
